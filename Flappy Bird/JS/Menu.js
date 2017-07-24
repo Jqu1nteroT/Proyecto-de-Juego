a6ef4364ebf55f2d976621ee4a574c52b819214e
Jqu1nteroT <juanpabloquintero25@gmail.com>
2017-07-24 14:51:16 +0200

    Copia final
    Resultado final del juego flappy bird, totalmente funcional salvo algunos bugs que serán resueltos posteriomente

---
+++ Flappy Bird/JS/Menu.js
@@ -0,0 +1,33 @@
+//creamos una variable que contendra el estado menu
+var Menu = {
+	//Recursos a cargar
+	preload: function () {
+		//definicmos un color de fondo
+		juego.stage.backgroundColor = '#fff';
+		//cargamos el boton de play 
+		juego.load.image('boton', 'sprites/play.png');
+	},
+	//muestra los recursos previamente cargados
+	create: function () {
+		//mostramos el boton previamnete cargado
+		var boton = this.add.button(juego.width/2, juego.height/2, 'boton', this.iniciarJuego, this);
+			//Centramos el boton con el punto de anclaje
+			boton.anchor.setTo(0.5);
+			//ajustamos la talla del boton para que se vea mas agradable
+			boton.scale.setTo(0.5,0.5);
+		//Agregamos texto a la pantalla de inicio del juego	
+		var txtIniciar = juego.add.text(juego.width/2, juego.height/2 -85, "Iniciar Juego", {font: "bold 24px sans-serif", fill:"black", align: "center"});
+			//cambiamos el punto de anclaje del texto
+			txtIniciar.anchor.setTo(0.5);
+		//Agregamos titulo a la pantalla de inicio del juego	
+		var txtTitulo = juego.add.text(juego.width/2, juego.height/2 -125, "Flappy Bird (Copy)", {font: "bold 30px sans-serif", fill:"green", align: "center"});
+			//cambiamos el punto de anclaje del texto
+			txtTitulo.anchor.setTo(0.5);
+	},
+	//Con esta funcion inicializamos el estado de juego
+	iniciarJuego: function () {
+		this.state.start('Game')
+		//this.state.start('estadoPrincipal')
+	}
+
+};
---
+++ Flappy Bird/JS/estados.js
@@ -0,0 +1,14 @@
+//Condiciones para iniciar la construccion del juego
+var juego = new Phaser.Game(370, 496, Phaser.CANVAS, 'bloque_juego');
+
+
+//Estados posibles que tedra el juego
+//estado menu
+juego.state.add('Menu', Menu);
+//Estado juego activo
+juego.state.add('Game', accion);
+//juego.state.add('perro', estadoPrincipal);
+//Estado juego terminado
+juego.state.add('Game_Over', Game_over);
+//estado inicial en el que comenzara el juego
+juego.state.start('Menu');
